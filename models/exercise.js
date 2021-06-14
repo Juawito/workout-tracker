e47c42ef234734be75bfed2c1ae8c69356d22017
Juawito <santiagojuan311@gmail.com>
2021-06-14 12:42:52 -0400

    creating exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -1,4 +1,38 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const {Schema} = mongoose;
 
-const exerciseSchema = new Schema({})
+const exerciseSchema = new Schema({
+    // name type weight sets reps duration distance 
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter the name of the new exercise.",
+    },
+    type: {
+        type: String,
+        required: 'Enter what type of workout this is.'
+    },
+    weight: {
+        type: Number,
+        default: null,
+    },
+    sets: {
+        type: Number,
+        default: null,
+    },
+    reps: {
+        type: Number,
+        default: null,
+    },
+    duration: {
+        type: Number,
+        default: null,
+    },
+    distance: {
+        type: Number,
+        default: null,
+    }
+})
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
