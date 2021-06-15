19333ec49e12614d0d2278909a5bf557ab355839
Juawito <santiagojuan311@gmail.com>
2021-06-15 18:33:04 -0400

    fixing path issues with server file

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
-app.user(express.json());
+app.use(express.json());
 
-app.user(express.static("public"));
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker', {
     useNewUrlParser: true,
     useFindAndModify: true,
 })
 
-app.use(require('./routes/api.js'));
+app.use(require('./routes/index.js'));
 
 app.listen(PORT, () => {
     console.log(`App running on port: ${PORT}` );
